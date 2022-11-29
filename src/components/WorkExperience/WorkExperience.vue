e6609e2d89b8d54c742902c700663c1b3fb1e89f
terrypan1 <terrypan1@gmail.com>
2022-11-29 22:58:57 +0800

    11/29/2300

---
+++ src/components/WorkExperience/WorkExperience.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+<template>
+    <div class="container">
+        <div class="progress">
+            <ul class="progress-ul">
+                <li class="circle">2011</li>
+                <li class="circle y2017">2017</li>
+                <li class="circle y2020">2020</li>
+                <li class="circle y2022">2022</li>
+                <li class="circle now">Now</li>
+            </ul>
+            <div class="circleBar1"></div>
+            <div class="circleBar2"></div>
+            <div class="circleBar3"></div>
+            <div class="circleBar4"></div>
+        </div>
+        <div class="aboutMe">
+
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+@import '../../assets/styles/WorkExperience.scss';
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Resume from '../../components/Resume/Resume.vue'
 import Content from '../../components/Content/Content.vue'
+import WorkExperience from '../../components/WorkExperience/WorkExperience.vue'
 </script>
 
 <template>
 <Resume></Resume>
 <Content></Content>
+<WorkExperience></WorkExperience>
 </template>
 
 <style lang="scss" scoped>
